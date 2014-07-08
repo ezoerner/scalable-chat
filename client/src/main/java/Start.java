2b83f7711551b8feb9dd25ff5f70e4571b784bb9
Eric Zoerner <eric.zoerner@gmail.com>
2014-07-08 22:46:58 +0200

    Added Start java class to facilitate native bundling of application

---
+++ client/src/main/java/Start.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2014 Eric Zoerner
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import scalable.client.Main$;
+
+/**
+ * Need a real static main method as entry point to help with packaging as native app.
+ *
+ * @author Eric Zoerner <a href="mailto:eric.zoerner@gmail.com">eric.zoerner@gmail.com</a>
+ */
+@SuppressWarnings("ClassWithoutPackageStatement")
+public class Start {
+    private Start() {
+    }
+
+    public static void main(String[] args) {
+        Main$.MODULE$.main(args);
+    }
+}
